532a8547f7e1f2da2a233a04f2aa5fbbf6087e1a
Sarthak2916 <sarthaksharma2916@gmail.com>
2026-03-11 22:08:06 +0530

    Inch Measurement Equality

--- src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -16,12 +16,33 @@ public class QuantityMeasurementApp {
             return Double.compare(this.value,((Feet) obj).value)==0 ? true : false;
         }
     }
+    public static class Inches{
 
-    public static void main(String[] args) {
+        private final double value;
 
+        public Inches(double value){
+            this.value=value;
+        }
+        @Override
+        public boolean equals(Object obj){
+            if(obj==null) return false;
+            if(obj.getClass() != Inches.class) return false;
+            return Double.compare(this.value,((Inches) obj).value)==0 ? true : false;
+        }
+    }
+    public static void demonstrateFeetEquality(){
         Feet f1= new Feet(5.4);
         Feet f2= new Feet(5.4);
-
         System.out.println(f1.equals(f2));
+    }
+    public static void demonstrateInchesEquality(){
+        Inches i1= new Inches(3);
+        Inches i2= new Inches(3);
+        System.out.println(i1.equals(i2));
+    }
+    public static void main(String[] args) {
+
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
     }
 }
--- src/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -3,6 +3,7 @@ package com.apps.quantitymeasurement;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
 
@@ -33,6 +34,35 @@ public class QuantityMeasurementAppTest {
     @Test
     public void testFeetEquality_SameReference(){
         Feet f1= new Feet(1.0);
+        assertEquals(f1,f1);
+    }
+    @Test
+    public void testInchesEquality_SameValue(){
+        Inches f1= new Inches(1.0);
+        Inches f2= new Inches(1);
+        assertEquals(f1,f2);
+    }
+    @Test
+    public void testInchesEquality_DifferentValue(){
+        Inches f1= new Inches(5);
+        Inches f2= new Inches(4);
+        assertEquals(f1,f2);
+    }
+    @Test
+    public void testInchesEquality_NullComparison(){
+        Inches f1= new Inches(1.0);
+//        Inches f2= new Inches(1);
+        assertEquals(null,f1);
+    }
+    @Test
+    public void testInchesEquality_NonNumericInput(){
+        Inches f1= new Inches(1.0);
+        String s= "1.0";
+        assertEquals(f1,s);
+    }
+    @Test
+    public void testInchesEquality_SameReference(){
+        Inches f1= new Inches(1.0);
         assertEquals(f1,f1);
     }
 }
