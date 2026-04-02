61bc376f1a5c966ae712e9c2c11863701b30bec6
Sarthak2916 <sarthaksharma2916@gmail.com>
2026-04-02 09:16:44 +0530

    Database Integration with JDBC.

--- src/com/apps/quantitymeasurement/IQuantityMeasurementRepository.java
+++ src/com/apps/quantitymeasurement/IQuantityMeasurementRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 public interface IQuantityMeasurementRepository {
     void save(QuantityMeasurementEntity entity);
     List<QuantityMeasurementEntity> getAllMeasurements();
+
 }
